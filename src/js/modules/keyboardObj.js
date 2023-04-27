02c90a6c205838ed0c570f1f3f168a2ae6180bce
Maksym Voloshyn <mv.webdew@gmail>
2023-04-27 14:26:03 +0300

    feat: add а keyboard object module

---
+++ src/js/modules/keyboardObj.js
@@ -0,0 +1,460 @@
+export const KEYBOARD = {
+    rows: [
+        ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+        ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'],
+        ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+        ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
+        ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'],
+    ],
+    keys: {
+        Backquote: {
+            value: '`',
+            valuePressShift: '~',
+            valueRu: 'ё',
+            valuePressShiftRu: 'Ё',
+            styleCSS: null
+        },
+        Digit1: {
+            value: '1',
+            valuePressShift: '!',
+            valueRu: '1',
+            valuePressShiftRu: '!',
+            styleCSS: null
+        },
+        Digit2: {
+            value: '2',
+            valuePressShift: '@',
+            valueRu: '2',
+            valuePressShiftRu: '&quot;',
+            styleCSS: null
+        },
+        Digit3: {
+            value: '3',
+            valuePressShift: '#',
+            valueRu: '3',
+            valuePressShiftRu: '№',
+            styleCSS: null
+        },
+        Digit4: {
+            value: '4',
+            valuePressShift: '$',
+            valueRu: '4',
+            valuePressShiftRu: ';',
+            styleCSS: null
+        },
+        Digit5: {
+            value: '5',
+            valuePressShift: '%',
+            valueRu: '5',
+            valuePressShiftRu: '%',
+            styleCSS: null
+        },
+        Digit6: {
+            value: '6',
+            valuePressShift: '^',
+            valueRu: '6',
+            valuePressShiftRu: ':',
+            styleCSS: null
+        },
+        Digit7: {
+            value: '7',
+            valuePressShift: '&amp;',
+            valueRu: '7',
+            valuePressShiftRu: '?',
+            styleCSS: null
+        },
+        Digit8: {
+            value: '8',
+            valuePressShift: '*',
+            valueRu: '8',
+            valuePressShiftRu: '*',
+            styleCSS: null
+        },
+        Digit9: {
+            value: '9',
+            valuePressShift: '(',
+            valueRu: '9',
+            valuePressShiftRu: '(',
+            styleCSS: null
+        },
+        Digit0: {
+            value: '0',
+            valuePressShift: ')',
+            valueRu: '0',
+            valuePressShiftRu: ')',
+            styleCSS: null
+        },
+        Minus: {
+            value: '-',
+            valuePressShift: '_',
+            valueRu: '-',
+            valuePressShiftRu: '_',
+            styleCSS: null
+        },
+        Equal: {
+            value: '=',
+            valuePressShift: '+',
+            valueRu: '=',
+            valuePressShiftRu: '+',
+            styleCSS: null
+        },
+        Backspace: {
+            value: 'Backspace',
+            valuePressShift: 'Backspace',
+            valueRu: 'Backspace',
+            valuePressShiftRu: 'Backspace',
+            styleCSS: null
+        },
+        Tab: {
+            value: 'Tab',
+            valuePressShift: 'Tab',
+            valueRu: 'Tab',
+            valuePressShiftRu: 'Tab',
+            styleCSS: null
+        },
+        KeyQ: {
+            value: 'q',
+            valuePressShift: 'Q',
+            valueRu: 'й',
+            valuePressShiftRu: 'Й',
+            styleCSS: null
+        },
+        KeyW: {
+            value: 'w',
+            valuePressShift: 'W',
+            valueRu: 'ц',
+            valuePressShiftRu: 'Ц',
+            styleCSS: null
+        },
+        KeyE: {
+            value: 'e',
+            valuePressShift: 'E',
+            valueRu: 'у',
+            valuePressShiftRu: 'У',
+            styleCSS: null
+        },
+        KeyR: {
+            value: 'r',
+            valuePressShift: 'R',
+            valueRu: 'к',
+            valuePressShiftRu: 'К',
+            styleCSS: null
+        },
+        KeyT: {
+            value: 't',
+            valuePressShift: 'T',
+            valueRu: 'е',
+            valuePressShiftRu: 'Е',
+            styleCSS: null
+        },
+        KeyY: {
+            value: 'y',
+            valuePressShift: 'Y',
+            valueRu: 'н',
+            valuePressShiftRu: 'Н',
+            styleCSS: null
+        },
+        KeyU: {
+            value: 'u',
+            valuePressShift: 'U',
+            valueRu: 'г',
+            valuePressShiftRu: 'Г',
+            styleCSS: null
+        },
+        KeyI: {
+            value: 'i',
+            valuePressShift: 'I',
+            valueRu: 'ш',
+            valuePressShiftRu: 'Ш',
+            styleCSS: null
+        },
+        KeyO: {
+            value: 'o',
+            valuePressShift: 'O',
+            valueRu: 'щ',
+            valuePressShiftRu: 'Щ',
+            styleCSS: null
+        },
+        KeyP: {
+            value: 'p',
+            valuePressShift: 'P',
+            valueRu: 'з',
+            valuePressShiftRu: 'З',
+            styleCSS: null
+        },
+        BracketLeft: {
+            value: '[',
+            valuePressShift: '{',
+            valueRu: 'х',
+            valuePressShiftRu: 'Х',
+            styleCSS: null
+        },
+        BracketRight: {
+            value: ']',
+            valuePressShift: '}',
+            valueRu: 'ъ',
+            valuePressShiftRu: 'Ъ',
+            styleCSS: null
+        },
+        Backslash: {
+            value: '\'',
+            valuePressShift: '|',
+            valueRu: '\'',
+            valuePressShiftRu: '/',
+            styleCSS: null
+        },
+        Delete: {
+            value: 'Del',
+            valuePressShift: 'Del',
+            valueRu: 'Del',
+            valuePressShiftRu: 'Del',
+            styleCSS: null
+        },
+        CapsLock: {
+            value: 'CapsLock',
+            valuePressShift: 'CapsLock',
+            valueRu: 'CapsLock',
+            valuePressShiftRu: 'CapsLock',
+            styleCSS: null
+        },
+        KeyA: {
+            value: 'a',
+            valuePressShift: 'A',
+            valueRu: 'ф',
+            valuePressShiftRu: 'Ф',
+            styleCSS: null
+        },
+        KeyS: {
+            value: 's',
+            valuePressShift: 'S',
+            valueRu: 'ы',
+            valuePressShiftRu: 'Ы',
+            styleCSS: null
+        },
+        KeyD: {
+            value: 'd',
+            valuePressShift: 'D',
+            valueRu: 'в',
+            valuePressShiftRu: 'В',
+            styleCSS: null
+        },
+        KeyF: {
+            value: 'f',
+            valuePressShift: 'F',
+            valueRu: 'а',
+            valuePressShiftRu: 'А',
+            styleCSS: null
+        },
+        KeyG: {
+            value: 'g',
+            valuePressShift: 'G',
+            valueRu: 'п',
+            valuePressShiftRu: 'П',
+            styleCSS: null
+        },
+        KeyH: {
+            value: 'h',
+            valuePressShift: 'H',
+            valueRu: 'р',
+            valuePressShiftRu: 'Р',
+            styleCSS: null
+        },
+        KeyJ: {
+            value: 'j',
+            valuePressShift: 'J',
+            valueRu: 'о',
+            valuePressShiftRu: 'О',
+            styleCSS: null
+        },
+        KeyK: {
+            value: 'k',
+            valuePressShift: 'K',
+            valueRu: 'л',
+            valuePressShiftRu: 'Л',
+            styleCSS: null
+        },
+        KeyL: {
+            value: 'l',
+            valuePressShift: 'L',
+            valueRu: 'д',
+            valuePressShiftRu: 'Д',
+            styleCSS: null
+        },
+        Semicolon: {
+            value: ';',
+            valuePressShift: ':',
+            valueRu: 'ж',
+            valuePressShiftRu: 'Ж',
+            styleCSS: null
+        },
+        Quote: {
+            value: '&#39;',
+            valuePressShift: '&quot;',
+            valueRu: 'э',
+            valuePressShiftRu: 'Э',
+            styleCSS: null
+        },
+        Enter: {
+            value: 'Enter',
+            valuePressShift: 'Enter',
+            valueRu: 'Enter',
+            valuePressShiftRu: 'Enter',
+            styleCSS: null
+        },
+        KeyZ: {
+            value: 'z',
+            valuePressShift: 'Z',
+            valueRu: 'я',
+            valuePressShiftRu: 'Я',
+            styleCSS: null
+        },
+        KeyX: {
+            value: 'x',
+            valuePressShift: 'X',
+            valueRu: 'ч',
+            valuePressShiftRu: 'Ч',
+            styleCSS: null
+        },
+        KeyC: {
+            value: 'c',
+            valuePressShift: 'C',
+            valueRu: 'с',
+            valuePressShiftRu: 'С',
+            styleCSS: null
+        },
+        KeyV: {
+            value: 'v',
+            valuePressShift: 'V',
+            valueRu: 'м',
+            valuePressShiftRu: 'М',
+            styleCSS: null
+        },
+        KeyB: {
+            value: 'b',
+            valuePressShift: 'B',
+            valueRu: 'и',
+            valuePressShiftRu: 'И',
+            styleCSS: null
+        },
+        KeyN: {
+            value: 'n',
+            valuePressShift: 'N',
+            valueRu: 'т',
+            valuePressShiftRu: 'Т',
+            styleCSS: null
+        },
+        KeyM: {
+            value: 'm',
+            valuePressShift: 'M',
+            valueRu: 'ь',
+            valuePressShiftRu: 'Ь',
+            styleCSS: null
+        },
+        Comma: {
+            value: ',',
+            valuePressShift: '&lt;',
+            valueRu: 'б',
+            valuePressShiftRu: 'Б',
+            styleCSS: null
+        },
+        Period: {
+            value: '.',
+            valuePressShift: '&gt;',
+            valueRu: 'ю',
+            valuePressShiftRu: 'Ю',
+            styleCSS: null
+        },
+        Slash: {
+            value: '/',
+            valuePressShift: '?',
+            valueRu: '.',
+            valuePressShiftRu: ',',
+            styleCSS: null
+        },
+        ArrowUp: {
+            value: '&uarr;',
+            valuePressShift: '&uarr;',
+            valueRu: '&uarr;',
+            valuePressShiftRu: '&uarr;',
+            styleCSS: null
+        },
+        ShiftRight: {
+            value: 'Shift',
+            valuePressShift: 'Shift',
+            valueRu: 'Shift',
+            valuePressShiftRu: 'Shift',
+            styleCSS: null
+        },
+        ShiftLeft: {
+            value: 'Shift',
+            valuePressShift: 'Shift',
+            valueRu: 'Shift',
+            valuePressShiftRu: 'Shift',
+            styleCSS: null
+        },
+        ControlLeft: {
+            value: 'Ctrl',
+            valuePressShift: 'Ctrl',
+            valueRu: 'Ctrl',
+            valuePressShiftRu: 'Ctrl',
+            styleCSS: null
+        },
+        MetaLeft: {
+            value: 'Win',
+            valuePressShift: 'Win',
+            valueRu: 'Win',
+            valuePressShiftRu: 'Win',
+            styleCSS: null
+        },
+        AltLeft: {
+            value: 'Alt',
+            valuePressShift: 'Alt',
+            valueRu: 'Alt',
+            valuePressShiftRu: 'Alt',
+            styleCSS: null
+        },
+        Space: {
+            value: ' ',
+            valuePressShift: ' ',
+            valueRu: ' ',
+            valuePressShiftRu: ' ',
+            styleCSS: null
+        },
+        AltRight: {
+            value: 'Alt',
+            valuePressShift: 'Alt',
+            valueRu: 'Alt',
+            valuePressShiftRu: 'Alt',
+            styleCSS: null
+        },
+        ArrowLeft: {
+            value: '&larr;',
+            valuePressShift: '&larr;',
+            valueRu: '&larr;',
+            valuePressShiftRu: '&larr;',
+            styleCSS: null
+        },
+        ArrowDown: {
+            value: '&darr;',
+            valuePressShift: '&darr;',
+            valueRu: '&darr;',
+            valuePressShiftRu: '&darr;',
+            styleCSS: null
+        },
+        ArrowRight: {
+            value: '&rarr;',
+            valuePressShift: '&rarr;',
+            valueRu: '&rarr;',
+            valuePressShiftRu: '&rarr;',
+            styleCSS: null
+        },
+        ControlRight: {
+            value: 'Ctrl',
+            valuePressShift: 'Ctrl',
+            valueRu: 'Ctrl',
+            valuePressShiftRu: 'Ctrl',
+            styleCSS: null
+        }
+    },
+
+};

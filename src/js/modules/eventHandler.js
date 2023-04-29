bac64d69338f214cc003e2e9fb67555e299c1ea6
Maksym Voloshyn <mv.webdew@gmail>
2023-04-29 20:20:35 +0300

    feat: add shift button behavior on virtual and physical keyboard

--- src/js/modules/eventHandler.js
+++ src/js/modules/eventHandler.js
@@ -37,9 +37,11 @@ const keyboardState = {
 };
 
 export function addEventHandler(keyboardObj) {
-  const { keysChar, keysShift, keys } = keyboardObj;
+  const { keysChar, keys } = keyboardObj;
   const textArea = document.querySelector('.textarea');
   const buttons = document.querySelectorAll('.key');
+  const shiftLeft = document.getElementById('ShiftLeft');
+  const shiftRight = document.getElementById('ShiftRight');
 
   textArea.addEventListener('blur', (e) => {
     e.preventDefault();
@@ -49,21 +51,59 @@ export function addEventHandler(keyboardObj) {
 
   document.addEventListener('click', (e) => {
     let target = e.target;
+
+    if (target.id == 'ShiftLeft' || target.id == 'ShiftRight') {
+      if (keyboardState.isShiftPress) {
+        if (target.classList.contains('key--active')) {
+          target.classList.remove('key--active');
+          changeCharValue(keys, buttons, keyboardState);
+          keyboardState.unPressKey(target.id);
+        } else {
+          if (target.id == 'ShiftLeft') {
+            shiftLeft.classList.add('key--active');
+            shiftRight.classList.remove('key--active');
+          } else {
+            shiftRight.classList.add('key--active');
+            shiftLeft.classList.remove('key--active');
+          }
+        }
+      } else {
+        changeCharValue(keys, buttons, keyboardState);
+        keyboardState.pressKey(target.id);
+
+        if (target.id == 'ShiftLeft') {
+          shiftLeft.classList.add('key--active');
+        } else {
+          shiftRight.classList.add('key--active');
+        }
+      }
+
+    }
+
     for (let i = 0; i < buttons.length; i += 1) {
       if (target.id == buttons[i].id) {
 
         if (keysChar.includes(buttons[i].id)) {
           editTextAreaData(textArea, buttons[i]);
+
+          if (keyboardState.isShiftPress) {
+            changeCharValue(keys, buttons, keyboardState);
+            keyboardState.unPressKey('ShiftLeft');
+            shiftLeft.classList.remove('key--active');
+            shiftRight.classList.remove('key--active');
+          }
         }
       }
     }
 
-    console.log(keyboardState.isAltPress);
+
+    console.log(keyboardState.isShiftPress);
   });
+
 
   textArea.addEventListener('keydown', (e) => {
     e.preventDefault();
-
+    console.log('blaaaaaaa');
     for (let i = 0; i < buttons.length; i += 1) {
       if (e.code == buttons[i].id) {
         buttons[i].classList.add('key--active');
@@ -72,7 +112,9 @@ export function addEventHandler(keyboardObj) {
           editTextAreaData(textArea, buttons[i]);
         } else {
           if (e.code == 'ShiftLeft' || e.code == 'ShiftRight') {
-            changeCharValue(keys, keysShift, buttons);
+            if (!keyboardState.isShiftPress)
+              changeCharValue(keys, buttons, keyboardState);
+            keyboardState.pressKey(e.code);
           }
         }
       }
@@ -81,10 +123,16 @@ export function addEventHandler(keyboardObj) {
 
   textArea.addEventListener('keyup', (e) => {
     e.preventDefault();
+
     for (let i = 0; i < buttons.length; i += 1) {
       if (e.code == buttons[i].id) {
         buttons[i].classList.remove('key--active');
       }
+    }
+
+    if (e.code == 'ShiftLeft' || e.code == 'ShiftRight') {
+      changeCharValue(keys, buttons, keyboardState);
+      keyboardState.unPressKey(e.code);
     }
   });
 }

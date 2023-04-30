e3652d9cef292ab107deab2612f86121a1dbe282
Maksym Voloshyn <mv.webdew@gmail>
2023-04-30 17:57:05 +0300

    fix: change the number of arguments to addEventHadler()

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import { KEYBOARD } from './modules/keyboardObj.js';
 import { createPage } from './modules/createPage.js';
+import { keyboardState } from './modules/keyboardState.js';
 import { addEventHandler } from './modules/eventHandler.js';
-
 createPage(KEYBOARD);
 
-addEventHandler(KEYBOARD);
+addEventHandler(KEYBOARD, keyboardState);
--- src/js/modules/eventHandler.js
+++ src/js/modules/eventHandler.js
@@ -1,62 +1,6 @@
 import { editTextAreaData, changeCharValue, changeLetterCase, changeLanguage } from './buttonActions.js';
 
-const keyboardState = {
-  language: 'en',
-  isCapsPress: false,
-  isShiftPress: false,
-  isCtrlPress: false,
-  isAltPress: false,
-
-  pressKey(keyCode) {
-    if (keyCode == 'CapsLock') {
-      this.isCapsPress = this.isCapsPress ? false : true;
-    }
-    if (keyCode == 'ShiftLeft' || keyCode == 'ShiftRight') {
-      this.isShiftPress = true;
-    }
-    if (keyCode == 'ControlLeft' || keyCode == 'ControlRight') {
-      this.isCtrlPress = true;
-    }
-    if (keyCode == 'AltLeft' || keyCode == 'AlrRight') {
-      this.isAltPress = true;
-    }
-  },
-
-  unPressKey(keyCode) {
-    if (keyCode == 'ShiftLeft' || keyCode == 'ShiftRight') {
-      this.isShiftPress = false;
-    }
-    if (keyCode == 'ControlLeft' || keyCode == 'ControlRight') {
-      this.isCtrlPress = false;
-    }
-    if (keyCode == 'AltLeft' || keyCode == 'AlrRight') {
-      this.isAltPress = false;
-    }
-  },
-
-  changeLanguage() {
-    this.language = this.language == 'en' ? 'ru' : 'en';
-  },
-
-  saveLanguageToLocalStorage() {
-    localStorage.setItem('PinkPeppaPig', JSON.stringify(keyboardState));
-  },
-
-  setLanguage() {
-    if (localStorage.getItem('PinkPeppaPig')) {
-      const savedKeyboardState = JSON.parse(localStorage.getItem('PinkPeppaPig1'));
-      if (savedKeyboardState?.language) {
-        this.language = savedKeyboardState.language;
-      } else {
-        console.warn('Anyone else used PinkPeppaPig :-0 ?! Please clear localStorage!');
-      }
-    } else {
-      console.warn('Please enable localStorage in your browser settings!');
-    }
-  }
-};
-keyboardState.setLanguage();
-export function addEventHandler(keyboardObj) {
+export function addEventHandler(keyboardObj, keyboardState) {
   const { keysChar, keys } = keyboardObj;
   const textArea = document.querySelector('.textarea');
   const buttons = document.querySelectorAll('.key');
